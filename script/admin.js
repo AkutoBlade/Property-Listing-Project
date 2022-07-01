d4902251b0784903d1d301b61d9449fbdea36702
AkutoBlade <rared.isaacs@gmail.com>
2022-07-01 16:27:10 +0200

    Working

--- script/admin.js
+++ script/admin.js
@@ -1,3 +1,5 @@
+
+//Create Function
 const properties = JSON.parse(localStorage.getItem("property"));
 const propertiesBox= document.querySelector(".boxes")
 
@@ -6,7 +8,7 @@ createHTML(properties);
 function createHTML(props){
     propertiesBox.innerHTML = '';
     
-    props.forEach(sales => {
+    props.forEach((sales,index=0)=> {
         propertiesBox.innerHTML +=`    
         <div class="col-md-1">${sales.id}</div>
         <div class="col-md-2">${sales.address}</div>
@@ -16,10 +18,120 @@ function createHTML(props){
         </div>
         </div>
         <div class="col-md-1">$${sales.price}</div>
-        <div class="col-md-2">${sales.}</div>
-        <div class="col-md-2"></div>
-        <div class="col-md-1"></div>
-        <div class="col-md-2"></div>
- `   
+        <div class="col-md-2 text-center">${sales.bedroom}</div>
+        <div class="col-md-2 text-center">${sales.bathroom}</div>
+        <div class="col-md-1">${sales.land_size_ft}</div>
+        <div class="col-md-2"><i class="fa-regular fa-pen-to-square" onclick="editRecord(${index})" data-bs-toggle="modal" data-bs-target="#Modal2"></i>  <i class="fa-regular fa-trash-can" Onclick="deleteRecord(${index})"></i></div>
+ `    
     });
+
+   
 }
+
+const record =document.querySelector('#record');
+
+//Add Function
+document.getElementById("count").innerHTML= properties.length;
+
+function add(){
+    let street = document.querySelector('#street').value;
+    let image = document.querySelector('#urlimg').value;
+    let beds = document.querySelector('#bedNumb').value;
+    let baths = document.querySelector('#bathNumb').value;
+    let price = document.querySelector('#price').value;
+    let landsize = document.querySelector('#landsize').value;
+
+    properties.push(
+        {
+            id: properties.length+1,
+            address: street,
+            image: image,
+            price: price,
+            bedroom: beds,
+            bathroom: baths,
+            land_size_ft: landsize
+        }
+    )
+    localStorage.setItem("property", JSON.stringify(properties));
+    createHTML(properties);
+}
+record.addEventListener('click',add);
+
+//Delete Function
+function deleteRecord(id) {
+    if(id > -1) {
+        properties.splice(id, 1);
+        for (let i = 0; i < properties.length; i++) {
+            properties[i].id = i+1;
+        }
+        localStorage.setItem("property",JSON.stringify(properties))
+    }
+        createHTML(properties);
+
+        console.log(properties)
+}
+
+//Sort Function
+
+let asc = true;
+
+function sortByAddress(){
+    asc = !asc;
+    let sortBy= properties.sort((a,b)=>{
+        let f1 = a.address;
+        let f2= b.address;
+
+        console.log(f1)
+        if(f1 < f2){
+            return -1;
+        }
+        if(f1 > f2){
+        return 1
+        }
+        return 0;
+    });
+
+    if (asc == false) {
+        document.querySelector("#icSort").innerHTML = `<i class="fa-solid fa-arrow-down-a-z"></i>`;
+        createHTML(sortBy);
+      } else if (asc == true) {
+        document.querySelector("#icSort").innerHTML = `<i class="fa-solid fa-arrow-down-z-a"></i>`;
+        sortBy.reverse();
+        createHTML(sortBy);
+      }
+}
+
+//Edit Function
+
+let ids= 0;
+function editRecord(id){
+    let newland = properties.find(edt => edt.id -1 === id)
+    document.querySelector('#street2').value = newland.address;
+    document.querySelector('#urlimg2').value = newland.image;
+    document.querySelector('#bedNumb2').value = newland.bedroom;
+    document.querySelector('#bathNumb2').value = newland.bathroom;
+    document.querySelector('#price2').value = newland.price;
+    document.querySelector('#landsize2').value = newland.land_size_ft;
+    ids = id;
+}
+function editAdd(){
+    let newProp = JSON.parse(localStorage.getItem('property'));
+    let newland = newProp.find(edt => edt.id -1 === ids)
+    let street = document.querySelector('#street2').value;
+    let image = document.querySelector('#urlimg2').value;
+    let beds = document.querySelector('#bedNumb2').value;
+    let baths = document.querySelector('#bathNumb2').value;
+    let price = document.querySelector('#price2').value;
+    let landsize = document.querySelector('#landsize2').value;
+    newland.address = street;
+    newland.image = image;
+    newland.bedroom = beds;
+    newland.bathroom = baths;
+    newland.price = price;
+    newland.land_size_ft = landsize;
+    properties[ids] = newland;
+    localStorage.setItem('property',JSON.stringify(properties));
+    createHTML(properties);
+}
+document.querySelector('#editProperty').addEventListener('click',editAdd);
+
--- script/code.js
+++ script/code.js
@@ -1,122 +1,125 @@
-localStorage.setItem("property", JSON.stringify(property = [{
-        id: 1, 
-        address: "960 Kolokolo Place",
-        image: "1.jpg",
-        price: 1450000,
-        bedroom: 4,
-        bathroom: 3,
-        land_size_ft: 5431
-    },
-    {
-        id: 2,
-        image: "2.jpg",
-        address: "1220 Mokuhano Street",
-        price: 1525000,
-        bedroom: 4,
-        bathroom: 2,
-        land_size_ft: 5500
-    },
-    {
-        id: 3,
-        address: "709 Hahaione Street",
-        image: "3.jpg",
-        price: 1612000,
-        bedroom: 4,
-        bathroom: 3,
-        land_size_ft: 6540
-    },
-    {
-        id: 4,
-        address: "741 Kapulena Loop",
-        image: "4.jpg",
-        price: 1850000,
-        bedroom: 5,
-        bathroom: 3,
-        land_size_ft: 8117
-    },
-    {
-        id: 5,
-        address: "775 Ainapo Street",
-        image: "5.jpg",
-        price: 1850000,
-        bedroom: 4,
-        bathroom: 2,
-        land_size_ft: 5929
-    },
-    {
-        id: 6,
-        address: "775 Ainapo Street",
-        image: "6.jpg",
-        price: 1875000,
-        bedroom: 4,
-        bathroom: 3,
-        land_size_ft: 7508
-    },
-    {
-        id: 7,
-        address: "1106 Kaluanui Road",
-        image: "7.jpg",
-        price: 1900000,
-        bedroom: 5,
-        bathroom: 4,
-        land_size_ft:  6157
-    },
-    {
-        id: 8,
-        address: " 1020 Hoa Street",
-        image: "8.jpg",
-        price: 2368000,
-        bedroom: 3,
-        bathroom: 2,
-        land_size_ft: 12200
-    },
-    {
-        id: 9,
-        address: "280 Poipu Drive",
-        image: "9.jpg",
-        price: 11900000,
-        bedroom: 6,
-        bathroom: 5,
-        land_size_ft:  21539
-    },
-    {
-        id: 10,
-        address: "6721 Hawaii Kai Drive",
-        image: "10.jpg",
-        price: 2588000,
-        bedroom: 4,
-        bathroom: 2,
-        land_size_ft: 8951
-    },
-    {
-        id: 11,
-        address: "1220 Mokuhano Street",
-        image: "11.jpg",
-        price: 1850000,
-        bedroom: 4,
-        bathroom: 2,
-        land_size_ft:  19215
-    },
-    {
-        id: 12,
-        address: " 124 Hanohano Place",
-        image: "12.jpg",
-        price: 3388000,
-        bedroom: 5,
-        bathroom: 3,
-        land_size_ft: 11857
-    }
-]));
+
+// const property = [{
+//     id: 1,
+//     address: "960 Kolokolo Place",
+//     image: "1.jpg",
+//     price: 1450000,
+//     bedroom: 4,
+//     bathroom: 3,
+//     land_size_ft: 5431
+// },
+// {
+//     id: 2,
+//     image: "2.jpg",
+//     address: "1220 Mokuhano Street",
+//     price: 1525000,
+//     bedroom: 4,
+//     bathroom: 2,
+//     land_size_ft: 5500
+// },
+// {
+//     id: 3,
+//     address: "709 Hahaione Street",
+//     image: "3.jpg",
+//     price: 1612000,
+//     bedroom: 4,
+//     bathroom: 3,
+//     land_size_ft: 6540
+// },
+// {
+//     id: 4,
+//     address: "741 Kapulena Loop",
+//     image: "4.jpg",
+//     price: 1850000,
+//     bedroom: 5,
+//     bathroom: 3,
+//     land_size_ft: 8117
+// },
+// {
+//     id: 5,
+//     address: "775 Ainapo Street",
+//     image: "5.jpg",
+//     price: 1850000,
+//     bedroom: 4,
+//     bathroom: 2,
+//     land_size_ft: 5929
+// },
+// {
+//     id: 6,
+//     address: "775 Ainapo Street",
+//     image: "6.jpg",
+//     price: 1875000,
+//     bedroom: 4,
+//     bathroom: 3,
+//     land_size_ft: 7508
+// },
+// {
+//     id: 7,
+//     address: "1106 Kaluanui Road",
+//     image: "7.jpg",
+//     price: 1900000,
+//     bedroom: 5,
+//     bathroom: 4,
+//     land_size_ft: 6157
+// },
+// {
+//     id: 8,
+//     address: " 1020 Hoa Street",
+//     image: "8.jpg",
+//     price: 2368000,
+//     bedroom: 3,
+//     bathroom: 2,
+//     land_size_ft: 12200
+// },
+// {
+//     id: 9,
+//     address: "280 Poipu Drive",
+//     image: "9.jpg",
+//     price: 11900000,
+//     bedroom: 6,
+//     bathroom: 5,
+//     land_size_ft: 21539
+// },
+// {
+//     id: 10,
+//     address: "6721 Hawaii Kai Drive",
+//     image: "10.jpg",
+//     price: 2588000,
+//     bedroom: 4,
+//     bathroom: 2,
+//     land_size_ft: 8951
+// },
+// {
+//     id: 11,
+//     address: "1220 Mokuhano Street",
+//     image: "11.jpg",
+//     price: 1850000,
+//     bedroom: 4,
+//     bathroom: 2,
+//     land_size_ft: 19215
+// },
+// {
+//     id: 12,
+//     address: " 124 Hanohano Place",
+//     image: "12.jpg",
+//     price: 3388000,
+//     bedroom: 5,
+//     bathroom: 3,
+//     land_size_ft: 11857
+// }
+// ]
+
+// localStorage.setItem("property", JSON.stringify(property));
 
 const properties = JSON.parse(localStorage.getItem("property"));
-const propertiesBox= document.getElementById("properties")
+const propertiesBox = document.getElementById("properties")
 
 createHTML(properties);
 
-function createHTML(props){
+function createHTML(props) {
     propertiesBox.innerHTML = '';
     props.forEach(sales => {
-        propertiesBox.innerHTML +=`
+        propertiesBox.innerHTML += `
          <div class="row" id="row">
         <div id="${sales.id}" style="background:url(./image/${sales.image}) no-repeat center center/cover" class="sizes">
          <div class="text">
@@ -130,42 +133,40 @@ function createHTML(props){
          </div>
          </div>
         </div>
-        <div></div>
-        </div>`   
+        </div>`
     });
 }
 
-function bedrooms(value1){
-    let filteredItems = properties.filter(x=>{
-        if(x>=5){
-         return   x.bedroom >= value1
+function bedrooms(value1) {
+    let filteredItems = properties.filter(x => {
+        if (x >= 5) {
+            return x.bedroom >= value1
+        } else {
+            return x.bedroom == value1
         }
-        else{
-          return   x.bedroom == value1
-        }
-    
+
     });
     createHTML(filteredItems);
 
 }
-function bathrooms(value2){
 
-    let filteredItems = properties.filter(x=>{
-        return  x.bathroom == value2
+function bathrooms(value2) {
+
+    let filteredItems = properties.filter(x => {
+        return x.bathroom == value2
         // if(x>=5){
         //     return   x.bathroom >= value
         //    }
         //    else{
-            
+
         //    }
     });
     createHTML(filteredItems);
 }
 
-function price(value3){
-    let filteredItems = properties.filter(x=>{
-        return x.price <= value3 
+function price(value3) {
+    let filteredItems = properties.filter(x => {
+        return x.price <= value3
     });
     createHTML(filteredItems);
 }
-
